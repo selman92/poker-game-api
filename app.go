e4a411e3bd2831acd4ba6ba0e278ef03ba2bea0c
Selman Genç <slmn.gnc@gmail.com>
2022-08-05 00:06:35 +0300

    Added env file and default port.

--- app.go
+++ app.go
@@ -3,21 +3,36 @@ package main
 import (
 	"PokerGameAPI/domain/deck"
 	"PokerGameAPI/server"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	server := server.NewServer(deck.NewDeckMemoryRepository())
+	LoadEnvironmentVariables()
+
+	s := server.NewServer(deck.NewDeckMemoryRepository())
 
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
-	router.Get("/api/create", server.CreateDeck)
-	router.Get("/api/open", server.OpenDeck)
-	router.Get("/api/draw", server.DrawCards)
+	router.Get("/api/create", s.CreateDeck)
+	router.Get("/api/open", s.OpenDeck)
+	router.Get("/api/draw", s.DrawCards)
 
-	log.Fatal(http.ListenAndServe(":6592", router))
+	fmt.Println("Server started listening on port: " + os.Getenv("PORT"))
+
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+}
+
+func LoadEnvironmentVariables() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.4.0
 	github.com/stretchr/testify v1.8.0
 )
 
